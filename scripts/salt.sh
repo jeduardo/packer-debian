4947d0c1f8ddadc0d4f915f2bc649060194ae8ce
J. Eduardo <j.eduardo@gmail.com>
2015-10-07 23:20:09 +0200

    Added scripts for serial port support and salt.

---
+++ scripts/salt.sh
@@ -0,0 +1,5 @@
+apt-get install -y salt-minion
+service salt-minion stop
+sed -i "s/#master: salt/master: mothership.local/" /etc/salt/minion
+service salt-minion start
+userdel -f -r streetlamp
---
+++ scripts/serial.sh
@@ -0,0 +1,7 @@
+systemctl enable serial-getty@ttyS0.service
+systemctl start serial-getty@ttyS0.service
+
+sed "s/quiet/console=tty0 console=ttyS0,115200n8/" -i /etc/default/grub 
+echo "GRUB_TERMINAL=serial" >> /etc/default/grub
+echo "GRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1\"" >> /etc/default/grub
+update-grub
