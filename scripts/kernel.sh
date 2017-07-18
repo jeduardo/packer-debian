a80bc0b3f7ec61df561f0fb706b398884fc2fc0d
J. Eduardo <j.eduardo@gmail.com>
2017-07-18 22:15:56 +0200

    Changing serial provisioning to kernel provisioning. Ensuring that network names continue to be ethX.

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,7 +1,7 @@
 systemctl enable serial-getty@ttyS0.service
 systemctl start serial-getty@ttyS0.service
 
-sed "s/quiet/console=tty0,ttyS0,115200n8/" -i /etc/default/grub 
+sed "s/quiet/console=tty0,ttyS0,115200n8 net.ifnames=0/" -i /etc/default/grub 
 echo "GRUB_TERMINAL=serial" >> /etc/default/grub
 echo "GRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1\"" >> /etc/default/grub
 update-grub
